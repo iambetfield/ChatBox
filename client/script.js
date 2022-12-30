6ca66d109b6a111b9c98649b547867513b494329
Nalvarez <nmalvarez@gmail.com>
2022-12-30 15:07:25 -0300

    url update

--- client/script.js
+++ client/script.js
@@ -98,7 +98,7 @@ const handleSubmit = async (e) =>{
   // fetch data desde el server => para tener la respuesta del bot
 
   // primer parametro con el local host, segundo parámetro con todas las opciones
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatbox-jdw3.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
